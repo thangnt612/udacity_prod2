9130b7ec13fc1f1b631d0cbaf571fcc21e83e351
THAVICO-JMFBEGR\Admin <thangnt612@gmail.com>
2022-09-07 09:35:02 +0700

    a

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,7 +1,7 @@
 #--------- Flask settings
 SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
 SERVER_PORT = 5000
-FLASK_DEBUG = True # Do not use debug mode in prod
+FLASK_DEBUG = False # Do not use debug mode in prod
 
 # Flask-Restplus settings
 SWAGGER_UI_DOC_EXPANSION = 'list'
